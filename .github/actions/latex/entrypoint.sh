25c60dd3704bb2e45d0d63bc60f2ba1206ac4647
HidetoNiwa <tototohide@gmail.com>
2020-01-31 17:01:37 +0900

    タグを日時に変更

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -10,6 +10,10 @@ platex ./0_main/main.tex
 
 dvipdfmx -f ptex-ipa.map main
 
+timedatectl set-timezone Asia/Tokyo
+
+today=`date "+%Y%m%d%H%M%S"`
+
 # create release
 res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
 -d "
@@ -25,6 +29,6 @@ res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com
 rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
 
 # upload built pdf
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=main.pdf\
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${today}/assets?name=main.pdf\
   --header 'Content-Type: application/pdf'\
   --upload-file main.pdf
